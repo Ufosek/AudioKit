23b55d799775cb0bc347febb2986d838fe74c098
pszacin <ufoscoder@gmail.com>
2019-02-11 16:38:04 +0100

    Microphone init fix

--- AudioKit/Common/Nodes/Input/AKMicrophone.swift
+++ AudioKit/Common/Nodes/Input/AKMicrophone.swift
@@ -8,9 +8,9 @@
 
 /// Audio from the standard input
 open class AKMicrophone: AKNode, AKToggleable {
-
+    
     internal let mixer = AVAudioMixerNode()
-
+    
     /// Output Volume (Default 1)
     @objc open dynamic var volume: Double = 1.0 {
         didSet {
@@ -18,7 +18,7 @@ open class AKMicrophone: AKNode, AKToggleable {
             mixer.outputVolume = Float(volume)
         }
     }
-
+    
     /// Set the actual microphone device
     public func setDevice(_ device: AKDevice) throws {
         do {
@@ -27,20 +27,20 @@ open class AKMicrophone: AKNode, AKToggleable {
             AKLog("Could not set input device")
         }
     }
-
+    
     fileprivate var lastKnownVolume: Double = 1.0
-
+    
     /// Determine if the microphone is currently on.
     @objc open dynamic var isStarted: Bool {
         return volume != 0.0
     }
-
+    
     /// Initialize the microphone
-    override public init() {
+    @objc override public init() {
         super.init()
         self.avAudioNode = mixer
         AKSettings.audioInputEnabled = true
-
+        
         #if os(iOS)
         let format = getFormatForDevice()
         // we have to connect the input at the original device sample rate, because once AVAudioEngine is initialized, it reports the wrong rate
@@ -52,11 +52,11 @@ open class AKMicrophone: AKNode, AKToggleable {
         AudioKit.engine.inputNode.connect(to: self.avAudioNode)
         #endif
     }
-
+    
     deinit {
         AKSettings.audioInputEnabled = false
     }
-
+    
     private func setAVSessionSampleRate(sampleRate: Double) {
         #if !os(macOS)
         do {
@@ -66,14 +66,14 @@ open class AKMicrophone: AKNode, AKToggleable {
         }
         #endif
     }
-
+    
     /// Function to start, play, or activate the node, all do the same thing
     @objc open func start() {
         if isStopped {
             volume = lastKnownVolume
         }
     }
-
+    
     /// Function to stop or bypass the node, both are equivalent
     @objc open func stop() {
         if isPlaying {
@@ -81,19 +81,25 @@ open class AKMicrophone: AKNode, AKToggleable {
             volume = 0
         }
     }
-
+    
     // Here is where we actually check the device type and make the settings, if needed
     private func getFormatForDevice() -> AVAudioFormat? {
+        let audioFormat: AVAudioFormat?
         #if os(iOS) && !targetEnvironment(simulator)
         let currentFormat = AudioKit.engine.inputNode.inputFormat(forBus: 0)
         let desiredFS = AudioKit.deviceSampleRate
-        return AVAudioFormat(commonFormat: currentFormat.commonFormat,
-                             sampleRate: desiredFS,
-                             interleaved: currentFormat.isInterleaved,
-                             channelLayout: currentFormat.channelLayout!)
+        if let layout = currentFormat.channelLayout {
+            audioFormat = AVAudioFormat(commonFormat: currentFormat.commonFormat,
+                                        sampleRate: desiredFS,
+                                        interleaved: currentFormat.isInterleaved,
+                                        channelLayout: layout)
+        } else {
+            audioFormat = AVAudioFormat(standardFormatWithSampleRate: desiredFS, channels: 2)
+        }
         #else
         let desiredFS = AKSettings.sampleRate
-        return AVAudioFormat(standardFormatWithSampleRate: desiredFS, channels: 2)
+        audioFormat = AVAudioFormat(standardFormatWithSampleRate: desiredFS, channels: 2)
         #endif
+        return audioFormat
     }
 }
